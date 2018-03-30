976837187bcdcb469099729d18d9c90999632091
Vikram Waradpande <wvikram11@gmail.com>
2018-03-30 11:51:42 +0530

    Added decision tree classifier

--- src/dt.py
+++ src/dt.py
@@ -1,10 +1,36 @@
 import numpy as np
-from sklearn import preprocessing, model_selection, neighbors
+from sklearn import preprocessing, model_selection, tree
 import pandas as pd
 
 
 dataFrame = pd.read_csv('/Users/Vikram/Documents/Projects/Data-Mining-Breast-Cancer/data/breast-cancer-wisconsin.data',
                        names = ['id','clump_thickness', "unif_cell_size", "unif_cell_shape", "marg_adhesion", "single_epith_cell_size", "bare_nuclei", "bland_chrom", "norm_nucleoli", "mitoses", "class"])
 
-dataFrame.replace('?','-9999999', inplace=True)
+
+#Drop missing data values
+dataFrame.replace('?',np.nan, inplace=True)
+dataFrame.dropna(0,'any',inplace=True)
+
+
+#Drop the ID column
 dataFrame.drop(['id'],1,inplace=True)
+
+
+#Separate attributes and class
+X = np.array(dataFrame.drop('class',1))
+y = np.array(dataFrame['class'])
+
+#Set up 10 fold cross-validation
+kf = model_selection.KFold(n_splits = 10);
+globalAccuracy = 0.0
+
+
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #print(train_index)
+    print("--------")
+    clf_gini = tree.DecisionTreeClassifier(criterion="gini",max_depth=20,min_samples_leaf=30)
+    clf_gini.fit(X_train, y_train)
+    accuracy = clf_gini.score(X_test, y_test)
+    print(accuracy)
